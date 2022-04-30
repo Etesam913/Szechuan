c56d3afcd26c636b4073fc0e4d6b73bae0a243f3
Mikee <michaelwolfsonkin@gmail.com>
2022-04-30 10:09:22 -0400

    Added cabinet

--- Assets/Scripts/OpenCloseDoor.cs
+++ Assets/Scripts/OpenCloseDoor.cs
@@ -1,13 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class OpenCloseFridge : MonoBehaviour
+public class OpenCloseDoor : MonoBehaviour
 {
     private bool doorOpen = false;
 
-    private Vector3 CLOSED_ANGLES;
-    private Vector3 OPEN_ANGLES;
+    [SerializeField] private Vector3 CLOSED_ANGLES;
+    [SerializeField] private Vector3 OPEN_ANGLES;
 
     private int interpolationFramesCount = 35;
     private int elapsedFrames;
@@ -16,8 +15,6 @@ public class OpenCloseFridge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CLOSED_ANGLES = new Vector3(0.0f,0.0f,0.0f);
-        OPEN_ANGLES = new Vector3(0.0f,-90.0f,0.0f);
         elapsedFrames = interpolationFramesCount;
     }
 
@@ -28,14 +25,14 @@ public class OpenCloseFridge : MonoBehaviour
         {
             float interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
 
-            doorAxis.eulerAngles = doorOpen ? Vector3.Lerp(CLOSED_ANGLES, OPEN_ANGLES,   interpolationRatio) : 
+            doorAxis.localEulerAngles = doorOpen ? Vector3.Lerp(CLOSED_ANGLES, OPEN_ANGLES,   interpolationRatio) : 
                                               Vector3.Lerp(OPEN_ANGLES,   CLOSED_ANGLES, interpolationRatio);
 
             elapsedFrames++;
         }
     }
 
-    public void ToggleFridgeDoor()
+    public void ToggleDoor()
     {
         if(elapsedFrames > interpolationFramesCount)
         {
