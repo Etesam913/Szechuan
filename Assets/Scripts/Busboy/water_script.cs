3be7e8669a9fd993af1d087082a8d467303f85ba
Vaddi Sai Satwik <vsaisatwik1996@gmail.com>
2022-04-30 15:03:40 -0400

    Busboy_Level_updates_with_XRDirect_Grab

--- Assets/Scripts/Busboy/water_script.cs
+++ Assets/Scripts/Busboy/water_script.cs
@@ -16,21 +16,29 @@ public class water_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        updates_time += 1;
-        if (((updates_time % 90) == 0) || ((updates_time % 91) == 0) || ((updates_time % 92) == 0) ||
-            ((updates_time % 93) == 0) || ((updates_time % 94) == 0) || ((updates_time % 95) == 0) ||
-            ((updates_time % 96) == 0) || ((updates_time % 97) == 0) || ((updates_time % 98) == 0) ||
-            ((updates_time % 99) == 0) || ((updates_time % 100) == 0))
-
+        if (lerpcolor.is_water_on || lerpcolor_mug.is_water_on)
         {
-            animator.SetBool("water_anim", false);
-            sphere.GetComponent<MeshRenderer>().enabled = true;
+            updates_time += 1;
+            if (((updates_time % 90) == 0) || ((updates_time % 91) == 0) || ((updates_time % 92) == 0) ||
+                ((updates_time % 93) == 0) || ((updates_time % 94) == 0) || ((updates_time % 95) == 0) ||
+                ((updates_time % 96) == 0) || ((updates_time % 97) == 0) || ((updates_time % 98) == 0) ||
+                ((updates_time % 99) == 0) || ((updates_time % 100) == 0))
+
+            {
+                animator.SetBool("water_anim", false);
+                sphere.GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                animator.SetBool("water_anim", true);
+                sphere.GetComponent<MeshRenderer>().enabled = false;
+            }
+            if (updates_time % 100 == 0)
+            {
+                updates_time = 0;
+            }
         }
         else {
-            animator.SetBool("water_anim", true);
-            sphere.GetComponent<MeshRenderer>().enabled = false;
-        }
-        if (updates_time % 100 == 0) {
             updates_time = 0;
         }
     }
---
+++ Assets/Scripts/lerpcolor_mug.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpcolor_mug : MonoBehaviour
+{
+    public Color startColor;
+    public Color endColor;
+    public float speed = 1.0f;
+    float startTime;
+    static public bool is_water_on = false;
+    int count = 0;
+    float t = 0;
+    float t_old = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        GetComponent<Renderer>().material.color = startColor;
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (is_water_on)
+        {
+            t = ((Time.time - startTime) / 5) + t_old;
+            GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "WaterOn")
+        {
+            t_old = t;
+            is_water_on = true;
+            count = (2 * count) + 1;
+            startTime = Time.time;
+            other.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+        if (other.gameObject.tag == "floor_busboy")
+        {
+            if (LGrab.hold == false && RGrab.hold == false)
+            {
+                GetComponent<BoxCollider>().isTrigger = false;
+                GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "WaterOn")
+        {
+            is_water_on = false;
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+    }
+}
