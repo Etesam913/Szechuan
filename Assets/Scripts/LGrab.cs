93ba8d04d793bd3133692ca34b635f7975e397cb
Etesam913 <etesamansari13@gmail.com>
2022-05-02 23:45:24 -0400

    🔥 Removed the random blue cylinder

--- Assets/Scripts/LGrab.cs
+++ Assets/Scripts/LGrab.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public class LGrab : MonoBehaviour
 {
     static public bool hold = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
